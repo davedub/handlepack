6f69cc7d797a28cd8fbad08fb3d8376f78fdaadd
davedub <dwitus@gmail.com>
2018-01-27 22:34:07 -0800

    add sass and postcss

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
+const scss = require('./app.scss');
+const css = require('./styles.sss');
 console.log("Hello from app.js again!!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: './src/app.js',
@@ -7,10 +7,25 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [
+            {
+            test: /\.sss$/,
+            use: ExtractTextPlugin.extract([ 'css-loader', 'postcss-loader' ])
+            },
+            {test: /\.scss$/, 
+            use: ExtractTextPlugin.extract([ 'css-loader', 'sass-loader'])
+            }]
+    },
     plugins: [
         new HtmlWebpackPlugin({
           title: 'Webpack2 Tutorial',
+          minify: {
+              collapseWhitespace: true
+          },
+          hash: true,
           template: './src/index.ejs', // Load a custom template (lodash by default see the FAQ for details)
-        })
+        }),
+        new ExtractTextPlugin("app.css")
       ]
 }
