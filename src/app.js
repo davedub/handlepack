6dfebea18b41cfb7af68c68cd12d99149fa60622
davedub <dwitus@gmail.com>
2018-01-28 13:48:58 -0800

    treating hbs helpers like partials for moment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 const scss = require('./app.scss');
 const css = require('./styles.sss');
+
 console.log("Hello from app.js again!!");
 
-var randomnum = require('./helpers/randomnum.js');
-var salutation = require('./helpers/salutation.js');
-var whisper = require('./helpers/whisper.js');
--- src/helpers/whisper.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function(msg) {
-    return msg.toLowerCase;
-};
